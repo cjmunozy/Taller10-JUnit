34fa10fa8dd0f3e5113abc9caec168bb3591b19c
cjmunozy <cjmunoz@espol.edu.ec>
2024-01-15 12:53:45 -0500

    Agregando código base

---
+++ Sección A/src/prueba/TestCalculation.java
@@ -0,0 +1,12 @@
+package prueba;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;;
+
+public class TestCalculation {
+	@Test
+	public void testFindMax() {
+		assertEquals(4, Calculation.findMax(new int[] {1, 3, 4, 2}));
+		assertEquals(-1, Calculation.findMax(new int[] {-12, - 1, -3, -4, -2}));
+	}
+}
