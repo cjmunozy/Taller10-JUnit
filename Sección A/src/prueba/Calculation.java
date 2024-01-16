c97d7e217b9ad3cc34fe3dcb9ba0165624ea876c
cjmunozy <cjmunoz@espol.edu.ec>
2024-01-15 19:32:28 -0500

    Corregida la lógica del método findMax()

--- Sección A/src/prueba/Calculation.java
+++ Sección A/src/prueba/Calculation.java
@@ -2,7 +2,7 @@ package prueba;
 
 public class Calculation {
 	public static int findMax(int arr[]){
-		int max = Integer.MIN_VALUE;
+		int max = arr[0];
 		for(int i = 1; i < arr.length; i++){
 			if(max < arr[i])
 				max = arr[i];
